62dfbdfde9dde351f45ac5e2f13c15e9d23603e7
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-26 11:52:26 +0000

    feat: add initial template

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, utils, nixpkgs }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ghc
+            ormolu
+            haskell-language-server
+          ];
+        };
+      }
+    );
+}
