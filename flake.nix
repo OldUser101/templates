8813f97d67db9570ffcb67da33355bd387a29a1b
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-26 14:26:38 +0000

    feat: split haskell into separate template

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
+  description = "Nathan Gill's flake templates";
 
-  outputs = { self, utils, nixpkgs }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            ghc
-            ormolu
-            haskell-language-server
-          ];
-        };
-      }
-    );
+  outputs =
+    { self }:
+    let
+      genTitle = name: "Nathan Gill's ${name} flake template";
+    in
+    {
+      templates.haskell = rec {
+        path = ./haskell;
+        description = genTitle "Haskell";
+        welcomeText = ''
+          # ${description}
+
+          This is a simple Haskell template containing:
+
+          - Glasgow Haskell Compiler (GHC)
+          - Ormolu (formatter)
+          - haskell-language-server (LSP)
+
+          No further help is provided, as none of these are complicated
+          or require additional setup.
+        '';
+      };
+    };
 }
---
+++ haskell/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nathan Gill's Haskell flake template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      utils,
+      nixpkgs,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ghc
+            ormolu
+            haskell-language-server
+          ];
+        };
+      }
+    );
+}
